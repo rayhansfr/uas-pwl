b0fdaad4a2dbdb7bb11ab44fa0757e93bab85578
ummu athiya <ummuathiyyah05@gmail.com>
2024-01-14 12:00:10 +0700

    updating footer

--- components/footer.php
+++ components/footer.php
@@ -1,29 +1,29 @@
-<!-- FOOTER GOES HERE -->
 <footer class="text-center text-white footer-sm">
     <div class="container p-4 pb-0">
         <section class="mb-4">
+            <div class="mb-4">
+                <h4>Yayasan Al-Kainah Assabiqunal Awwalun</h4>
+            </div>
 
-            <a class="btn btn-outline-light btn-floating m-1" href="#!" role="button">
-                <i class="fab fa-facebook-f"></i>
-            </a>
-            <a class="btn btn-outline-light btn-floating m-1" href="#!" role="button">
-                <i class="fab fa-twitter"></i>
-            </a>
-            <a class="btn btn-outline-light btn-floating m-1" href="#!" role="button">
-                <i class="fab fa-google"></i>
-            </a>
-            <a class="btn btn-outline-light btn-floating m-1" href="#!" role="button">
-                <i class="fab fa-instagram"></i>
-            </a>
-            <a class="btn btn-outline-light btn-floating m-1" href="#!" role="button">
-                <i class="fab fa-linkedin-in"></i>
-            </a>
-            <a class="btn btn-outline-light btn-floating m-1" href="#!" role="button">
-                <i class="fab fa-github"></i>
-            </a>
+            <div id="map-container-google-1" class="mb-4">
+                <iframe src="https://www.google.com/maps/embed?pb=!1m17!1m12!1m3!1d3964.4535174964903!2d106.86541497399246!3d-6.464086593527524!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m2!1m1!2zNsKwMjcnNTAuNyJTIDEwNsKwNTInMDQuOCJF!5e0!3m2!1sen!2sid!4v1705207526266!5m2!1sen!2sid" width="100%" height="200" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+            </div>
+
+            <div>
+                <a class="btn btn-outline-light btn-floating m-1" href="tel:+6282127050296" role="button">
+                    <i class="fas fa-phone"></i>
+                </a>
+                <a class="btn btn-outline-light btn-floating m-1" href="https://wa.me/6282127050296" role="button">
+                    <i class="fab fa-whatsapp"></i>
+                </a>
+                <a class="btn btn-outline-light btn-floating m-1" href="mailto:admin@assabiqunal-cibinong.com" role="button">
+                    <i class="fas fa-envelope"></i>
+                </a>
+            </div>
         </section>
     </div>
-    
+
+    <!-- Copyright -->
     <div class="text-center p-3" style="background-color: #1a1a1a;">
         © 2024 Copyright: Hilmi, Athiya & Rayhan.
     </div>
